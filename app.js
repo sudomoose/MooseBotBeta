9d19de9ed07e73dcb1013d0d9dd3356035bc51f6
BuildTools <unconfigured@null.spigotmc.org>
2016-12-20 16:00:20 -0800

    Possibly fixed $ping command

--- app.js
+++ app.js
@@ -8,30 +8,21 @@ const region = require("./region.json"); //loads region.json
 client.on('ready', () => {
   console.log('MooseBot init successful');
   console.log(`Currently serving ${bot.guilds.size} servers`);
-  client.setGame(`Fast Response Times for ${bot.guilds.size}`, "http://www.twitch.tv/monstercat");
+  client.setGame()
   console.log(`[${new Date()}]:MooseBot init successful`);
 });
+
+
 client.on('message', message => { //commands
   if(message.author.bot) return; //checks if bot user (self) is author
   if(!message.content.startsWith(config.prefix)) return; //check if message starts with config.prefix
 
-
-  let command = message.content.split(" ")[0]; //removes config.prefix from message to make command creation if statements much easier to create
-  command = command.slice(config.prefix.length); //^^^
+  let command = message.content.split(" ")[0]; 
+  command = command.slice(config.prefix.length);
 
   let args = message.content.split(" ").slice(1); //allows and creates arguments
 
-  if (command === "sum") {
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p+c);
-    message.channel.sendMessage(total)
-  } else  //adds any numbers stated after $add together and sends message of answer
-
-  if(command === "say") {
-        message.channel.sendMessage("${message.user} says: " + args.join(" "));
-    } else //sends any text after "say " to channel
-
-  if(command === "ping") { //returns "pong"
+  if(command === "ping") {
         message.channel.sendMessage(`Pong! \`${Date.now - message.createdTimeStamp} ms\``);
     } else
 
@@ -40,10 +31,9 @@ client.on('message', message => { //commands
     } else  //sends about info to channel
 
   if(command === "purge") { //purges ammount stated after "purge " of messages
-        message.user.sendMessage("You Do Not Have The Required Permissions to do this!");
-        message.channel.sendMessage("This feature is not yet complete! Please check back later!");
-          message.channel.sendMessage("This feature is not yet complete! Please check back later!");
+console.log("purge command used, not yet complete");
       } else
+
       if (command === "help") {
       message.channel.sendMessage("this feature is not yet finished! sorry");
     }
@@ -61,6 +51,7 @@ if(region.config === "EU") {
   client.login(token.NA);
   console.log("Now Acting as MooseBot NA");
 }
+
 
 client.on('GuildMemberAdd', member => {
   let guild = member.guild;
